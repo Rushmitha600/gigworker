d4f73490dc7b3142f8a5b209a64662261df3fea6
Your Name <your.email@example.com>
2026-03-20 13:57:24 +0530

    made

--- frontend/script.js
+++ frontend/script.js
@@ -224,6 +224,7 @@ function updateUserLocations() {
     const loc2 = document.getElementById('settings-location2').value.trim();
     const loc3 = document.getElementById('settings-location3').value.trim();
     
+    
     const locations = [];
     if (loc1) locations.push(loc1);
     if (loc2) locations.push(loc2);
